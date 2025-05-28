3798f0a1e0410a39341c3cb83f3c56362116988d
absakran01 <443015155@sm.imamu.edu.sa>
2025-05-28 11:32:44 +0300

    added more DAO methods
    yet to be tested

--- src/main/java/com/absakran/capstone_project/repositories/ProductDAOImpl.java
+++ src/main/java/com/absakran/capstone_project/repositories/ProductDAOImpl.java
@@ -2,7 +2,8 @@
 
     import java.util.List;
 
-    import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
     import com.absakran.capstone_project.entities.Product;
 
@@ -42,11 +43,63 @@ import jakarta.transaction.Transactional;
             em.persist(product);
         }
 
+        @Override
+        @Transactional
+        public void buyProduct(Product product, int quantity) {
+            // TODO Auto-generated method stub
+            if (product == null) {
+                throw new IllegalArgumentException("Product cannot be null");
+            }
+            Product temp = getProductById(product.getId());
+            temp.setStock(temp.getStock()-quantity);
+            em.persist(temp);
+        }
+
+        @Override
+        public Product getProductById(long id) {
+            // TODO Auto-generated method stub
+            if(id==0){
+                throw new IllegalArgumentException("Id cannot be null");
+            }
+            return em.find(Product.class, id);
+        }
+
+        @Override
+        public List<Product> getProductsByKeyword(String keyword) {
+            // TODO Auto-generated method stub
+            if(keyword==null){
+                throw new IllegalArgumentException("Keyword cannot be null");
+            }
+            return em.
+            createQuery(
+                    "FROM Product p WHERE LOWER(p.name) LIKE :kw OR LOWER(p.description) LIKE :kw",
+                    Product.class
+                )
+                 .setParameter("kw", "%"+ keyword+ "%")
+                 .getResultList();
+        }
+
+        @Override
+        @Transactional
+        public void restockProduct(Product product, int restock) {
+            // TODO Auto-generated method stub
+            if(product==null){
+                throw new IllegalArgumentException("Product cannot be null");
+            }
+
+            Product temp = em.find(Product.class, product.getId());
+            temp.setStock(temp.getStock()+restock);
+            em.persist(temp);
+
+        }
+
+        
+
+
+        // DI 
         public ProductDAOImpl(EntityManager em){
             this.em = em;
             System.out.println("ProductDAOImpl constructor called");
         }
-
-        
 
     }
